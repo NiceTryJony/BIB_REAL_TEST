70bf259cf5e9624fd68fab9019743da8f9ea63db
Danil <gol.daniel30@gmail.com>
2024-12-20 23:56:11 +0100

    Update biblioteka_zss_V3.sql

--- biblioteka_zss2.sql
+++ biblioteka_zss2.sql
@@ -1,35 +1,33 @@
--- Adminer 4.8.1 MySQL 10.4.32-MariaDB dump
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `aktualnosci`;
-CREATE TABLE `aktualnosci` (
+DROP TABLE IF EXISTS `konkursy`;
+CREATE TABLE `konkursy` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tytul` text NOT NULL,
-  `tresc` longtext NOT NULL,
+  `tytul` varchar(255) NOT NULL,
+  `tresc` varchar(255) NOT NULL,
   `data` date NOT NULL,
-  `nazwy_zdjec` longtext NOT NULL,
+  `nazwy_zdjec` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
-INSERT INTO `aktualnosci` (`id`, `tytul`, `tresc`, `data`, `nazwy_zdjec`) VALUES
-(1,	'Dyplom za udział w konkursie',	'Uczniowie 2B Ts dostali dyplomy za udział w konkursie szkolno/gminowym gdzie otrymali 1 miejsce',	'2024-11-10',	'Img'),
-(2,	'Dyplom niewiadomo za co',	'Nie pytaj o co tu chodzi. Musisz wiedzieć o tym',	'2024-11-12',	'');
+INSERT INTO `konkursy` (`id`, `tytul`, `tresc`, `data`, `nazwy_zdjec`) VALUES
+(1,'Dyplom za udział w konkursie','Uczniowie 2B Ts dostali dyplomy za udział w konkursie szkolno/gminowym gdzie otrymali 1 miejsce','2024-11-10','Img'),
+(2,'Dyplom niewiadomo za co','Nie pytaj o co tu chodzi. Musisz wiedzieć o tym','2024-11-12','');
 
 DROP TABLE IF EXISTS `konta`;
 CREATE TABLE `konta` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nazwa_uzytkownika` varchar(50) NOT NULL,
   `haslo` varchar(255) NOT NULL,
-  `uprawnienia` enum('admin','pelne','akt') NOT NULL DEFAULT 'akt',
+  `uprawnienia` enum('admin','pelne','akt') DEFAULT 'akt',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 INSERT INTO `konta` (`id`, `nazwa_uzytkownika`, `haslo`, `uprawnienia`) VALUES
-(1,	'gol.daniel30@gmail.com',	'$2y$10$X8Ug.l3nCJXEkV/y6NN15OE.ocHugFu9Rr0wi6ujeB0Mfrfj/9Bv2',	'admin');
+(1,'gol.daniel30@gmail.com','$2y$10$mfCKqwGqj6iJGHjKcFwyh.0jLHalVJ6pRw60Ki6eFcF81j82.RrR2','admin');
 
 DROP TABLE IF EXISTS `ksiazki`;
 CREATE TABLE `ksiazki` (
@@ -45,4 +43,26 @@ CREATE TABLE `ksiazki` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `wycieczki`;
+CREATE TABLE `wycieczki` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tytul` varchar(255) NOT NULL,
+  `tresc` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `nazwy_zdjec` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+
+DROP TABLE IF EXISTS `wyjazdy`;
+CREATE TABLE `wyjazdy` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tytul` varchar(255) NOT NULL,
+  `tresc` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `nazwy_zdjec` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 -- 2024-12-17 06:26:17
